29bbfac4705e9a447d3f8727e753fba6965c1407
nepenteh <nepenteh@gmail.com>
2023-01-05 07:59:11 +0000

    Correccin Hall Of Fame

--- src/main/java/com/jmrh/juego2048/HallOfFame.java
+++ src/main/java/com/jmrh/juego2048/HallOfFame.java
@@ -11,43 +11,45 @@ import java.io.Serializable;
  * @author josep
  */
 public class HallOfFame implements Serializable {
-    
+
+    public static final int TAMANIO = 10;
+
     private String nombres[];
     private int puntuaciones[];
     private int elementos;
-    
-        
+
     public HallOfFame() {
         elementos = 0;
-        nombres = new String[10];
-        puntuaciones = new int[10];
+        nombres = new String[TAMANIO];
+        puntuaciones = new int[TAMANIO];
     }
-    
+
     public void add(String nombre, int puntuacion) {
-        if(puntuacion>=puntuaciones[9]) {
-            puntuaciones[9] = puntuacion;
-            nombres[9] = nombre;
-        }
-        
-        int puntAux;
-        String nombreAux;
-        int i=8;
-        while(puntuaciones[i]<puntuaciones[i+1] && i>0) {
-            puntAux = puntuaciones[i];
-            nombreAux = nombres[i];
-            puntuaciones[i]=puntuaciones[i+1];
-            nombres[i]=nombres[i+1];
-            puntuaciones[i+1]=puntAux;
-            nombres[i+1]=nombreAux;
-            i--;
+        if (puntuacion >= puntuaciones[TAMANIO - 1]) {
+            puntuaciones[TAMANIO - 1] = puntuacion;
+            nombres[TAMANIO - 1] = nombre;
+
+            int puntAux;
+            String nombreAux;
+            int i = TAMANIO-2;
+            while (puntuaciones[i] < puntuaciones[i + 1] && i > 0) {
+                puntAux = puntuaciones[i];
+                nombreAux = nombres[i];
+                puntuaciones[i] = puntuaciones[i + 1];
+                nombres[i] = nombres[i + 1];
+                puntuaciones[i + 1] = puntAux;
+                nombres[i + 1] = nombreAux;
+                i--;
+            }
         }
     }
-    
+
     public void mostrar() {
         System.out.println("HALL OF FAME");
-        for(int i=0;i<10;i++) {
-            if(puntuaciones[i]>0) 
-                System.out.println(nombres[i]+" -> "+puntuaciones[i]);
+        for (int i = 0; i < 10; i++) {
+            if (puntuaciones[i] > 0) {
+                System.out.println(nombres[i] + " -> " + puntuaciones[i]);
+            }
         }
     }
 }
